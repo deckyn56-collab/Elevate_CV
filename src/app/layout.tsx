674cc79cd8ce303ea77190dee416a74aecc6d461
deckyn56-collab <deckyn56@gmail.com>
2026-08-01 19:59:55 +0700

    Fix favicon link tag in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang="id">
       <head>
         {/* Favicon Logo Anda */}
-        <link rel="icon" type="image/png" href="/favicon.png" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
         {children}
