0ccb48545ad624cb3d0a2b8b5f5bd19c11488e85
deckyn56-collab <deckyn56@gmail.com>
2026-08-01 23:56:14 +0700

    Fix layout.tsx formatting issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
       `
     }}
   />
-  />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
         {children}
