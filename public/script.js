21c8a0f0cdd798eb5081aeb9ea1c171bb00c00f5
deckyn56-collab <deckyn56@gmail.com>
2026-09-12 22:18:57 +0700

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -758,6 +758,8 @@ function openCvPreview() {
     // SVG Icon - Satu warna (Slate Gray)
     const ICON_COLOR = '#4b5563';
 
+  const iconBadge = '<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="' + ICON_COLOR + '" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:-2px;margin-right:6px;display:inline-block;"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="6"></circle><circle cx="12" cy="12" r="2"></circle></svg>';
+
     const iconPin = '<svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="' + ICON_COLOR + '" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:-2px;margin-right:5px;display:inline-block;"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>';
     
     const iconPhone = '<svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="' + ICON_COLOR + '" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:-2px;margin-right:5px;display:inline-block;"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>';
@@ -799,7 +801,7 @@ function openCvPreview() {
     const cvHTML = '<div class="cv-paper">' +
         '<div class="cv-header">' +
             '<div class="cv-name">' + name + '</div>' +
-            '<div class="cv-title">' + title + '</div>' +
+            '<div class="cv-title">' + iconBadge + ' ' + title + '</div>' +
             '<div class="cv-contact">' +
                 (address ? '<span>' + iconPin + ' ' + address + '</span>' : '') +
                 (phone ? '<span>' + iconPhone + ' ' + phone + '</span>' : '') +
